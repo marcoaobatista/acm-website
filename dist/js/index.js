f78b022a6337414216c0b87d5c11b3939c77264d
Caleb Kish <calalk@att.net>
2019-10-02 11:39:49 -0400

    Testing fetch API

--- dist/js/index.js
+++ dist/js/index.js
@@ -2,16 +2,46 @@ var ui = new Ui();
 
 
 // Send request to server for events
-var eventReq = new XMLHttpRequest();
-eventReq.open('GET', '/.netlify/functions/event');
-eventReq.responseType = 'json';
-eventReq.onload = function() {
-    var res = eventReq.response;
+// var eventReq = new XMLHttpRequest();
+// eventReq.open('GET', 'http://localhost:9000/.netlify/functions/event');
+// // eventReq.open('GET', '/.netlify/functions/event');
+// eventReq.responseType = 'json';
+// eventReq.onload = function() {
+//     var res = eventReq.response;
 
-    if (res.errors) {
-        console.log(res.errors);
-    } else {
-        var rawEvents = res;
+//     if (res.errors) {
+//         console.log(res.errors);
+//         return;
+//     }
+
+//     var rawEvents = res;
+
+//     console.log('RES:', res);
+
+//     var events = [];
+//     for (var i=0; i < rawEvents.length; i++)
+//         events.push(new AcmEvent(rawEvents[i]));
+    
+//     events.sort(function(event1, event2) {
+//         if (event1.past && (event1.date > event2.date)) return -1;
+//         if (event1.past && (event1.date < event2.date)) return 1;
+//     });
+
+//     for (var i=0; i < events.length; i++)
+//         ui.addEvent(events[i]);
+
+//     // Initialize the slider only AFTER the events are added to the UI
+//     ui.constructSlider('.glide');
+// }
+// eventReq.send();
+
+fetch('/.netlify/functions/event')
+// fetch('http://localhost:9000/.netlify/functions/event')
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+        
+        var rawEvents = data;
 
         var events = [];
         for (var i=0; i < rawEvents.length; i++)
@@ -27,26 +57,28 @@ eventReq.onload = function() {
 
         // Initialize the slider only AFTER the events are added to the UI
         ui.constructSlider('.glide');
-    }
-}
-eventReq.send();
+    });
 
 
 // Send request to server for board members
 var boardReq = new XMLHttpRequest();
+// boardReq.open('GET', 'http://localhost:9000/.netlify/functions/board');
 boardReq.open('GET', '/.netlify/functions/board');
 boardReq.responseType = 'json';
 boardReq.onload = function() {
     var res = boardReq.response;
 
+    console.log('RES:', res);
+
     if (res.errors) {
         console.log(res.errors);
-    } else {
-        var rawBoard = res;
-
-        for (var i=0; i < rawBoard.length; i++)
-            ui.addMember(rawBoard[i]);
+        return;
     }
+    
+    var rawBoard = res;
+
+    for (var i=0; i < rawBoard.length; i++)
+        ui.addMember(rawBoard[i]);
 }
 boardReq.send();
 
